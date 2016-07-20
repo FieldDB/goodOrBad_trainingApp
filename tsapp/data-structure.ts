ebe3e1f111de69f175c7c1898ad98739052ed30e
MatMath <mathieu.k.legault@gmail.com>
2016-07-20 16:59:35 -0400

    Updating the Structure of the page with the fetched data.

--- tsapp/data-structure.ts
+++ tsapp/data-structure.ts
@@ -2,17 +2,35 @@ export class GoldenRow {
 	filename: string;
 	url: string;
 	description: string;
-	criteria_target1: string;
+	criteria_array: string;
+	criteria_array_converted: number[];
 	creation_date: string;
 	passfail: boolean;
 	explanation: string;
 	type: string;
-	criteria_target2: number;
-	criteria_target3: number;
-	criteria_target4: number;
-	criteria_target5: number;
-	criteria_target6: number;
-	criteria_target7: number;
-	criteria_target8: number;
-	criteria_target9: number;
 }
+
+export class ResultValue {
+	username: string;
+	filenameid: number;
+	success: boolean;
+	fail_passed: boolean;
+	positive_failed: boolean;
+	delta_criteria_array: string;
+	delta_array: number[];
+	inspection_date: string;
+	user_comments: string;
+	type: string;
+	timeinsec: number;
+}
+
+exports.ArrOfCriteria = ["Criteria 1 to display",
+	"Criteria 2 to display",
+	"Criteria 3 to display",
+	"Criteria 4 to display",
+	"Criteria 5 to display",
+	"Criteria 6 to display",
+	"Long Criteria 7 to display the whole text for the user to see",
+	"Another Longer Criteria 8 to display the whole text for the user to see so he can make a decision.",
+	"Shorty",
+	"Should not have any slider since no critera were set"]
--- tsapp/pictpage/pictpage.ts
+++ tsapp/pictpage/pictpage.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { CORE_DIRECTIVES, NgIf } from '@angular/common';
+import { CORE_DIRECTIVES, NgIf, NgFor} from '@angular/common';
 import { Http, Response } from '@angular/http';
 
-import { GoldenRow } from 'tsapp/data-structure';
+import { GoldenRow, ResultValue, ArrOfCriteria } from 'tsapp/data-structure';
 import { CommService } from 'tsapp/pictpage/commService';
 
 @Component({
@@ -13,16 +13,69 @@ import { CommService } from 'tsapp/pictpage/commService';
 })
 
 export class Pictpage implements OnInit {
-	somejson: GoldenRow[];
+	imgToInspect: GoldenRow;
+	resultValue: ResultValue = {};
+	criterialist: string[];
 	error: any;
+	submited: boolean;
+	blockSubmit: boolean = true;
+	initialTimeStamp: number;
 
 	constructor(private commService: CommService){}
+	ngOnInit() {
+		this.resultValue.username = JSON.parse(localStorage.getItem("goodOrBadUser")).username;
+		this.resultValue.delta_array = [];
+		this.getOneImg();
+		this.criterialist = ArrOfCriteria;
+	}
 
 	getOneImg() {
+		this.submited = false; //we get a new img.
+		this.initialTimeStamp = Date.now();
+		this.resultValue.user_comments = "";
     this.commService.getDbImg()
-    				.subscribe((somejson:GoldenRow[])=>{this.somejson=somejson;});
-        // .then(somejson => this.somejson = somejson)
-        // .catch(error => this.error = error);
+    			.subscribe((arrayOf1:GoldenRow[])=>{
+    					// Get the Data from the server
+    					this.imgToInspect = arrayOf1[0];
+    					this.imgToInspect.criteria_array_converted = JSON.parse(arrayOf1[0].criteria_array);
+    					// Also populate the Default Result to send back at the end.
+    					this.resultValue.filenameid = this.imgToInspect.filename;  //This should be a OID or something unique.
+    					this.resultValue.type = this.imgToInspect.type; //This could be fetch directly in the SQL by joining table, but I dont like joint of big table for 1 value only.
+    			});
+  }
+
+  openImgInModal(url) {
+  	window.alert("opening img " + url);
+  }
+
+  setCriteriaX(index, target, value) {
+  	this.blockSubmit = false;
+  	if(!value) {
+  		value = Math.round(Math.random()*10); // just for testing until I have my slider setup
+  	}
+  	this.resultValue.delta_array[index] = target - value;
+  }
+
+  submitForm(result) {
+  	window.alert('Submitting the form (fakeing it, so fix me)');
+  	
+  	// Here submit the img and when we have feedback(promesses) we can call it submitted.
+  	this.resultValue.timeinsec = Math.round((Date.now() - this.initialTimeStamp)/1000); //Time in Second it took.
+  	this.resultValue.delta_criteria_array = JSON.stringify(this.resultValue.delta_array);
+  	if(result === this.imgToInspect.passfail) {
+  		// Both pass or both fail so It is all Good! The user took the good decision.
+  		this.resultValue.success = true;
+  	} else if (this.imgToInspect.passfail === true && result === false) {
+  		// Img was suppose to be good, but the user rejected it, then we have a Faillure and also a positive_failed
+  		this.resultValue.success = false;
+  		this.resultValue.positive_failed = true;
+  	} else if (this.imgToInspect.passfail === false && result === true) {
+  		// Img was suppose to be good, but the user rejected it, then we have a Faillure and also a positive_failed
+  		this.resultValue.success = false;
+  		this.resultValue.fail_passed = true;
+  	} 
+
+  	this.submited = true;
   }
 
 }
