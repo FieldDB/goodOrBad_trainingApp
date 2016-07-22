e1ed7da5707d1b4de8b4c8593100dc79cc0d8e6b
MatMath <mathieu.k.legault@gmail.com>
2016-07-22 12:19:08 -0400

    Integration the normalized option to have it Pass-Fail instead of a slider.

--- tsapp/pictpage/pictpage.ts
+++ tsapp/pictpage/pictpage.ts
@@ -19,7 +19,7 @@ export class Pictpage implements OnInit {
 	criterialist: CriteriaObject[];
 	error: any;
 	submited: boolean;
-	sliderStyle: boolean = true;
+	sliderStyle: boolean = false; //This is where we change the View to be a Slider Or a True/False buttons.
 	blockSubmit: boolean = true;
 	initialTimeStamp: number;
 
@@ -31,17 +31,36 @@ export class Pictpage implements OnInit {
 
 	getOneImg() {
 		// This start the Whole process again.
-		this.resultValue = {};
-		this.resultValue.username = JSON.parse(localStorage.getItem("goodOrBadUser")).username;
-		this.resultValue.delta_array = [];
+		this.resultValue = {
+			"username": JSON.parse(localStorage.getItem("goodOrBadUser")).username,
+			"filenameid": "string",
+			"success": null,
+			"fail_passed": null,
+			"positive_failed": null,
+			"delta_criteria_array": '',
+			"delta_array": [],
+			"inspection_date": '',
+			"user_comments": '',
+			"type": '',
+			"timeinsec": 0
+		};
 		this.submited = false; //we get a new img.
 		this.initialTimeStamp = Date.now();
-		this.resultValue.user_comments = "";
+		
     this.commService.getDbImg()
     			.subscribe((arrayOf1:GoldenRow[])=>{
     					// Get the Data from the server
     					this.imgToInspect = arrayOf1[0];
     					this.imgToInspect.criteria_array_converted = JSON.parse(arrayOf1[0].criteria_array);
+    					if(!this.sliderStyle) {
+    						// True false so I have to normalize them. Assomption here that the slider are 0-10. So Everything bellow 5 go to 0 and everything higher go to 1. 
+    						this.imgToInspect.criteria_array_converted = this.imgToInspect.criteria_array_converted.map(item => {
+    							if(item < 5) {
+    								return 0;
+    							} else {return 1;}
+    						});
+    					}
+    					this.imgToInspect
     					// Also populate the Default Result to send back at the end.
     					this.resultValue.filenameid = this.imgToInspect.filename;  //This should be a OID or something unique.
     					this.resultValue.type = this.imgToInspect.type; //This could be fetch directly in the SQL by joining table, but I dont like joint of big table for 1 value only.
@@ -53,12 +72,11 @@ export class Pictpage implements OnInit {
   }
 
   setCriteriaX(index, target, value) {
-  	this.blockSubmit = false;
-  	console.log("Selected value is: ",value);
-  	if(!value) {
-  		value = Math.round(Math.random()*10); // just for testing until I have my slider setup
+  	// Value or target can be 0
+  	if(!this.submited && target !== undefined && value !== undefined) {
+  		this.blockSubmit = false;
+  		this.resultValue.delta_array[index] = target - value;
   	}
-  	this.resultValue.delta_array[index] = target - value;
   }
 
   submitForm(result) {
@@ -83,19 +101,32 @@ export class Pictpage implements OnInit {
   	this.submited = true;
   }
 
-  sliderClass(value) {
+  sliderClass(delta : number) {
   	if(!this.submited) {
   		// Yes this should go in a service but it is soooo small it is a shame to put it away alone like a rejected function... 
-  		if(value === null || value === undefined) {return {"default_null_value": true}; }
+  		if(delta === null || delta === undefined) {return "default_null_value"; }
   	} else {
   	// TODO: on submit change the class Logic to set the class Green or Red depending if the user was succedful or Failed the criteria. (Check with tolerances)
-  		if(value === null || value === undefined) {return {"default_null_value": true}; }
-  		if(Math.abs(value) <= 1) {
-  			return {"slider_passing_value": true};
+  		if(delta === null || delta === undefined) {return "default_null_value"; }
+  		if(Math.abs(delta) <= 1) {
+  			return "slider_passing_value";
   		} else {
-  			return {"slider_failing_value": true};
+  			return "slider_failing_value";
   		}
   	}
+  }
+
+  btnClass(value:number, target:number) {
+  		// Yes this should go in a service but it is soooo small it is a shame to put it away alone like a rejected function... 
+		if(value === null || value === undefined) {
+			return "btn-info"; 
+		} else if (value === 1 && target === 1) {
+			return "btn-success";
+		} else if (value === 0 && target === 0) {
+			// This is conter intuitive, but it is because we have the Slider system also. that mean 1 is not always "Good" and 0 is not always "Bad" 
+			// it depend on what the user did input. and that will create a Delta value if the user failed. 
+			return "btn-danger";
+		}
   }
 
 }
